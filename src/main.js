222b644af0209b7579e5f3d1a4950d6091e9b147
高橋直輝 <takahashinaoki@takahashinaokinoMacBook-Pro.local>
2022-03-12 16:22:15 +0900

    ログイン/ログアウト機能の実装

--- src/main.js
+++ src/main.js
@@ -3,4 +3,11 @@ import App from "./App.vue"
 import router from "./router"
 import "./firebase"
 
+//グローバル変数の定義しようとした（できてない）
+// new Vue({
+//   beforeCreate: function () {
+//     console.log(this.$appName)
+//   },
+// })
+// Vue.prototype.$appName = "My App"
 createApp(App).use(router).mount("#app")
---
+++ src/views/SignInView.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="login">
+    <input type="email" v-model="email" />
+    <input type="password" v-model="password" />
+    <button @click="mailSignUp">アカウント新規作成</button>
+    <button @click="mailSignIn">ログイン</button>
+    <button @click="mailLogOut" v-bind:disabled="user == null">
+      ログアウト
+    </button>
+  </div>
+  <button @click="googleSignIn">googleSignIn</button>
+</template>
+
+<script>
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth"
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      user: null,
+    }
+  },
+  methods: {
+    googleSignIn() {
+      const auth = getAuth()
+      auth.languageCode = "ja"
+      const provider = new GoogleAuthProvider()
+      signInWithPopup(auth, provider)
+        .then((result) => {
+          const user = result.user
+          this.user = user
+          alert(user.email + "ログイン完了！")
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
+    mailSignIn() {
+      if (this.email.match("@")) {
+        const auth = getAuth()
+        auth.languageCode = "ja"
+        signInWithEmailAndPassword(auth, this.email, this.password)
+          .then((userCredential) => {
+            const user = userCredential.user
+            this.user = user
+            alert(this.user.email + "でログインしました")
+            // ...
+          })
+          .catch((error) => {
+            const errorCode = error.code
+            const errorMessage = error.message
+            alert(errorCode, errorMessage)
+          })
+      } else {
+        alert("正しいメールアドレスを記入してください")
+      }
+    },
+    mailSignUp() {
+      const auth = getAuth()
+      // auth.languageCode = "ja"
+      if (this.email.match("@")) {
+        createUserWithEmailAndPassword(auth, this.email, this.password)
+          .then((userCredential) => {
+            const user = userCredential.user
+            this.user = user
+            alert(this.user.email + "でサインアップしました")
+          })
+          .catch((err) => {
+            const errorCode = err.code
+            const errorMessage = err.message
+            alert(errorCode, errorMessage)
+          })
+      } else {
+        alert("正しいメールアドレスを記入してください")
+      }
+    },
+    mailLogOut() {
+      const auth = getAuth()
+      signOut(auth)
+        .then(() => {
+          alert("Sign-out successful.")
+        })
+        .catch((error) => {
+          alert(error.code, error.message)
+        })
+    },
+  },
+  computed() {
+    const auth = getAuth()
+    const user = auth.currentUser
+    this.user = user
+  },
+}
+</script>
