59fa873f70e46430c14b50d87696e3f532fed037
Ryota671 <rmdmatsuda@gmail.com>
2022-03-24 21:48:26 +0900

    入力欄を作成、投稿は出来ない

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 
+import SelfAnalysis from "../views/SelfAnalysis.vue"
 const routes = [
   {
     path: "/",
@@ -15,6 +16,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/self-analysis",
+    name: SelfAnalysis,
+    component: SelfAnalysis,
   },
 ]
 
