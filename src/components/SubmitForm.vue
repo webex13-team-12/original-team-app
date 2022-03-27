99d0fffefcfc01cd696e2ada850265ef8234b551
Ryota671 <rmdmatsuda@gmail.com>
2022-03-28 05:19:15 +0900

    投稿内容の共有実装

--- src/components/SubmitForm.vue
+++ src/components/SubmitForm.vue
@@ -5,21 +5,65 @@
       v-model="text"
       placeholder="幼少期～大学までの経験を共有しよう！"
     />
+
     <div class="form__buttons">
       <button v-on:click="postTweet" class="form__submit-button">投稿</button>
+    </div>
+    <div>
+      <p v-for="tweet in tweets" :key="tweet.id">
+        {{ tweet.text }}
+      </p>
     </div>
   </div>
 </template>
 
 <script>
+import {
+  collection,
+  addDoc,
+  serverTimestamp,
+  query,
+  orderBy,
+  limitToLast,
+  onSnapshot,
+} from "firebase/firestore"
+import { db } from "@/firebase"
+
 export default {
   data() {
     return {
       text: "",
+      tweets: [],
+      unsubscribe: null,
     }
   },
   methods: {
-    postTweet() {},
+    postTweet() {
+      addDoc(collection(db, "tweets"), {
+        text: this.text,
+        createdAt: serverTimestamp(),
+      })
+    },
+  },
+  created() {
+    const ref = query(
+      collection(db, "tweets"),
+      orderBy("createdAt"),
+      limitToLast(3)
+    )
+
+    this.unsubscribe = onSnapshot(ref, (snapshot) => {
+      let tweets = []
+      snapshot.forEach((doc) => {
+        tweets.push(doc.data())
+      })
+      this.tweets = tweets
+    })
+  },
+
+  unmounted() {
+    this.unsubscribe()
+    this.unsubscribe = null
   },
 }
 </script>
--- src/views/SelfAnalysis.vue
+++ src/views/SelfAnalysis.vue
@@ -3,37 +3,21 @@
     <div class="home__wrapper">
       <submit-form />
     </div>
-    <div>
-      <p v-for="tweet in tweets" :key="tweet.id">
-        {{ tweet.text }}
-      </p>
-    </div>
   </div>
 </template>
 
 <script>
 import SubmitForm from "../components/SubmitForm.vue"
-import { collection, addDoc } from "firebase/firestore"
-import { db } from "./firebase"
 
 export default {
+  components: { SubmitForm },
   data() {
     return {
       tweets: [],
     }
   },
   methods: {
-    postTweet() {
-      const tweet = {
-        text: "こんにちは、ツイートの本文です。",
-      }
-      addDoc(collection(db, "tweets"), tweet).then((ref) => {
-        this.tweets.push({
-          id: ref.id,
-          ...tweet,
-        })
-      })
-    },
+    postTweet() {},
   },
 }
 </script>
