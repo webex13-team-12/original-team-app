01ba89bdf3f9d72b7c97a6331d55e37ef3941a0d
MiyuOta <miyu.0219.macaron@gmail.com>
2022-03-29 04:52:22 +0900

    まーじ後の修正

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div class="top_box">
     <div>
       <p class="top_daimei">就活を、自分を、クリアにできるサイト</p>
@@ -167,100 +166,7 @@
         <p @click="mailLogOut"><a>ログアウト</a></p>
       </div>
     </div>
-
-  <div v-if="!user" class="form-wrapper">
-    <h1 v-if="isSignIn">Sign in</h1>
-    <h1 v-else>Create an account</h1>
-    <div class="toggle-radio">
-      <input @click="toSignIn(1)" type="radio" name="rdo" id="yes" checked />
-      <input @click="toSignIn(2)" type="radio" name="rdo" id="no" />
-      <div class="switch">
-        <label class="toggle-label-left" for="yes">Sign In</label>
-        <label class="toggle-label-right" for="no">Sign Up</label>
-        <span></span>
-      </div>
-    </div>
-    <form>
-      <div class="form-item">
-        <label for="email"></label>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email Address"
-          v-model="email"
-        />
-      </div>
-      <div class="form-item">
-        <label for="password"></label>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          v-model="password"
-        />
-      </div>
-      <div class="button-panel">
-        <input
-          type="button"
-          class="button"
-          title="Google Login"
-          value="Google Login"
-          @click="googleSignIn"
-        />
-        <input
-          v-if="isSignIn"
-          type="button"
-          class="button"
-          title="Sign In"
-          value="Sign In"
-          @click="mailSignIn"
-        />
-        <input
-          v-else
-          type="button"
-          class="signup-button"
-          title="Sign Up"
-          value="Sign Up"
-          @click="mailSignUp"
-        />
-      </div>
-    </form>
-    <div class="form-footer">
-      <p><a href="#">パスワードをお忘れの方</a></p>
-    </div>
   </div>
-  <div v-else class="form-wrapper">
-    <h1>Login User</h1>
-    <form>
-      <div class="form-item">
-        <label for="email"></label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email
-        Address"
-          v-model="user.email"
-        />
-      </div>
-      <div class="form-item">
-        <label for="password"></label>
-        <input
-          type="text"
-          name="password"
-          placeholder="displayName：未設定"
-          v-model="user.displayName"
-        />
-      </div>
-    </form>
-    <div class="form-footer">
-      <p @click="mailLogOut"><a>ログアウト</a></p>
-    </div>
-
-  </div>
-
-  <!-- この空画像は一旦srcのassetsに入ってる -->
-  <!-- <div class="top_daimei">就活を、自分を、クリアに</div> -->
-  <!-- <img src="@/assets/busy_woman.png" alt="悩み女性" /> -->
 </template>
 
 <script>
@@ -850,5 +756,4 @@ a.btn-malformation:hover {
   background-color: #59adc7;
   height: 80px;
 }
-
 </style>
